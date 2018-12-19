6e63d7eb1135746fd99959785d04d39cc61a70eb
Mischa Saunders <mischa.saunders@gmail.com>
2018-12-20 10:35:19 +1300

    update vars to lets

--- src/utilities/query-string.js
+++ src/utilities/query-string.js
@@ -1,9 +1,9 @@
 export default {
   parse: function parse(queryString) {
-    var query = {};
-    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-    for (var i = 0; i < pairs.length; i++) {
-      var pair = pairs[i].split('=');
+    let query = {};
+    let pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (let i = 0; i < pairs.length; i++) {
+      let pair = pairs[i].split('=');
       query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
     }
     return query;
