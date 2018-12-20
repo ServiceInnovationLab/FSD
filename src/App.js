17e9ff5f045047c5b2f62edde2360119511a2b17
danieljauch <daniel.p.jauch@gmail.com>
2018-12-20 13:56:33 +1300

    Found a potential solution to exact path matching

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
       <CategoryProvider>
         <CategoryContext.Consumer>
           {categoryContext => (
-            <Router>
+            <Router basename="/fsd-spike">
               <Fragment>
                 <Route
                   exact
