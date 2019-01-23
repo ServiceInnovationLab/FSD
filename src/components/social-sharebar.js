10a793b8ba83fe00e8abd7b8a6bf4a65bbe3553d
Lyall Morrison <lyall@rabidtech.co.nz>
2019-01-23 17:11:56 +1300

    Refactor social links

--- src/components/social-sharebar.js
+++ src/components/social-sharebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { stringify } from 'querystring';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faEnvelope
@@ -11,27 +12,48 @@ import {
 class Sharebar extends React.Component {
 
   render(){
-    let url = (this.props.url) ? this.props.url : window.location.href;
-    let subject = (this.props.subject) ? this.props.subject : 'Whānau%20Services%20in%20your%20area';
-    let description = (this.props.description) ? this.props.description : subject;
+    const { 
+      url = window.location.href, 
+      subject = document.title, 
+      description = document.title 
+    } = this.props
 
+    const socialLinks = [
 
-    let mailto = 'mailto:?subject='+subject+'&body='+url;
-    let facebook = 'https://www.facebook.com/sharer/sharer.php?u='+url;
-    let twitter = 'https://twitter.com/home?status='+description+':%20'+url;
-
+      <SocialLink
+        name='email'
+        url={`mailto:?${stringify({subject: subject, body: url})}`}
+        fa_icon={faEnvelope} />,
+        
+      <SocialLink
+        name='Facebook'
+        url={`https://www.facebook.com/sharer/sharer.php?${stringify({u: url})}`}
+        fa_icon={faFacebookSquare} />,
+        
+      <SocialLink
+        name='Twitter'
+        url={`https://twitter.com/home?${stringify({status: description+' '+url})}`}
+        fa_icon={faTwitterSquare} />,
+    ]
+    
     return (
       <div className="sharebar" aria-label="Sharebar">
         <div className="container">
           <p>Share via: </p>
           <ul className="sharebar-share">
-            <li><a href={mailto} title="Share by email"><FontAwesomeIcon icon={faEnvelope} />Email</a></li>
-            <li><a href={facebook} title="Share via Facebook"><FontAwesomeIcon icon={faFacebookSquare} />Facebook</a></li>
-            <li><a href={twitter} title="Share on twitter"><FontAwesomeIcon icon={faTwitterSquare} />Twitter</a></li>
+            { socialLinks.map(value => <li>{value}</li>)}
           </ul>
         </div>
       </div>
     );
+  }
+}
+
+class SocialLink extends React.Component {
+  render(){
+    const { name, url, fa_icon } = this.props
+
+    return <a href={url} title={`Share via ${name}`}><FontAwesomeIcon icon={fa_icon} />{name}</a>
   }
 }
 
