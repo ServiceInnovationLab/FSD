e53fb50e05caa56248bab1332f18c6be9dad08ae
Mischa Saunders <mischa.saunders@gmail.com>
2019-02-07 16:05:43 +1300

    remove autosuggest from search when field cleared

--- src/containers/auto-suggest.js
+++ src/containers/auto-suggest.js
@@ -67,6 +67,11 @@ export default class Example extends React.Component {
   };
 
   onSuggestionsClearRequested = () => {
+    const {updateSearchParams, autoSuggestOnChange} = this.props 
+
+    autoSuggestOnChange('')
+    updateSearchParams({ latitude: '', longitude: '', region: ''})
+
     this.setState({
       suggestions: []
     });
