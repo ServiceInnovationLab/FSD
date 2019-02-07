bdc0b90c5ce836e1923f8befba7d188830d3280a
Mischa Saunders <mischa.saunders@gmail.com>
2019-02-07 17:09:14 +1300

    whoops that was actually needs :/

--- src/containers/auto-suggest.js
+++ src/containers/auto-suggest.js
@@ -68,6 +68,8 @@ export default class Example extends React.Component {
 
   onSuggestionsClearRequested = () => {
     const {updateSearchParams, autoSuggestOnChange} = this.props 
+
+    autoSuggestOnChange('')
     updateSearchParams({ latitude: '', longitude: '', region: ''})
 
     this.setState({
