43bf4c7ed7acab602dc990a22812c4d26c490f50
Lyall Morrison <lyall@rabidtech.co.nz>
2019-02-12 16:21:27 +1300

    fix missing variable declaration

--- features/step_definitions/url-steps.js
+++ features/step_definitions/url-steps.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   this.Then(/^the URL "(\w+)" should be "([^"]+)"$/, async (attribute, value) => {
-    url = new URL(await driver.getCurrentUrl());
+    const url = new URL(await driver.getCurrentUrl());
     expect(url[attribute]).to.equal(value);
   });
 }
