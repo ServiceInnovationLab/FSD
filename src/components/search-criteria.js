7e692bcf587df82105e47e77ac068eb7334a2c37
JacOng17 <jacob_ong88@hotmail.com>
2019-03-27 11:14:36 +1300

    Refactored some lines of code.

--- src/components/search-criteria.js
+++ src/components/search-criteria.js
@@ -20,33 +20,14 @@ export default class SearchCriteria extends Component {
 function createSearchCriteria(keyword, address, region, category) {
   const search = ['Searching'];
 
-  if (keyword)
-    search.push(
-      <span>
-        {' '}
-        for: <b>{keyword}</b>{' '}
-      </span>,
+  if (keyword) search.push(
+      <span>{' '} for: <b>{keyword}</b>{' '}</span>,
     );
-  if (address)
-    search.push(
-      <span key={address}>
-        {' '}
-        near: <b>{address}</b>
-      </span>,
+  if (address || region) search.push(
+      <span key={address || region}>{' '} near: <b>{address || region}</b></span>,
     );
-    if (region)
-    search.push(
-      <span key={region}>
-        {' '}
-        near: <b>{region}</b>
-      </span>,
-    );
-  if (category)
-    search.push(
-      <span>
-        {' '}
-        in: <b>{category}</b>
-      </span>,
+  if (category) search.push(
+      <span>{' '} in: <b>{category}</b></span>,
     );
   if (search.length < 2) return null;
 
