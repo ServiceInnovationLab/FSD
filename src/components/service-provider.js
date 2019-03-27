89793c0b1b3902c28d72d81caf4eac2ae7df7e08
JacOng17 <jacob_ong88@hotmail.com>
2019-03-27 15:57:18 +1300

    Added feature to number results when needed.

--- src/components/service-provider.js
+++ src/components/service-provider.js
@@ -149,6 +149,7 @@ export default class ServiceProviders extends Component {
               More details
             </Link>
           )}
+          <hr />
         </footer>
       </section>
     );
