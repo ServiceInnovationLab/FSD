3d931d785797d9b469518bfc0fe586b5f741fd87
Lyall Morrison <lyall@rabidtech.co.nz>
2019-04-02 10:54:20 +1300

    tests for special keypress behaviour in address box

--- features/step_definitions/url-steps.js
+++ features/step_definitions/url-steps.js
@@ -1,9 +1,22 @@
 module.exports = function() {
   const URL = require('url').URL;
+  const parseQueryString = require('query-string').parse;
   
   this.Then(/^the URL "(\w+)" is "([^"]+)"$/, async (attribute, value) => {
     const url = new URL(await driver.getCurrentUrl());
     expect(url[attribute]).to.equal(value);
+  });
+
+  this.Then(/^the URL query has key "(\w+)" with value "([^"]+)"$/, async (k, v) => {
+    const url = new URL(await driver.getCurrentUrl());
+    const queries = parseQueryString(url['search']);
+    expect(queries[k]).to.equal(v);
+  });
+
+  this.Then(/^the URL query does not have key "(\w+)"$/, async k => {
+    const url = new URL(await driver.getCurrentUrl());
+    const queries = parseQueryString(url['search']);
+    expect(queries[k]).to.be.undefined
   });
 
   this.Then(/^I am on a service detail page$/, async () => {
