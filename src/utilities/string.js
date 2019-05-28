47625f1d89bea26f23a03392fb76861d8ed47100
Lyall Morrison <lyall@rabidtech.co.nz>
2019-05-28 13:52:29 +1200

    string truncation function

--- src/utilities/string.js
+++ src/utilities/string.js
@@ -1,3 +1,12 @@
 const stripSpaces = string => string.split(' ').join('');
 
-export { stripSpaces };
+// Truncate a string to a given length.
+//
+// Appends a terminator sequence (default ...) if necessary to show a truncation
+// has happened.
+const truncate = (string, target_length, terminator = '...') => 
+  string.length < target_length
+    ? string
+    : string.substring(0, target_length - terminator.length) + terminator;
+
+export { stripSpaces, truncate };
