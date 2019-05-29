c43eb67669328c479694a34cd9f40308fd3d3be6
Lyall Morrison <lyall@rabidtech.co.nz>
2019-05-29 17:14:44 +1200

    remove unused truncate function

--- src/utilities/string.js
+++ src/utilities/string.js
@@ -1,12 +1,3 @@
 const stripSpaces = string => string.split(' ').join('');
 
-// Truncate a string to a given length.
-//
-// Appends a terminator sequence (default ...) if necessary to show a truncation
-// has happened.
-const truncate = (string, target_length, terminator = '...') => 
-  string.length < target_length
-    ? string
-    : string.substring(0, target_length - terminator.length) + terminator;
-
-export { stripSpaces, truncate };
+export { stripSpaces };
