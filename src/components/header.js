72ddbfd4f3889f19715d34bc7a6b0ceebc87b737
Brenda <brenda@wallace.net.nz>
2019-05-31 15:22:09 +1200

    use jumbotron for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 const Header = () => {
   return (
     <header className="app__header">
       <div className="container">
-        <h1>Find Whānau Support - Mobile Site</h1>
-        <div>
-          <p>
+        <MDBJumbotron>
+          <h2 className="h1 display-3">
+            <strong>Find Whānau Support</strong>
+             <p className="text-muted">Mobile Site</p>
+           </h2>
+          <p className="lead">
             If you are using a mobile device and are looking for family support services near you then you can use Find
             Whānau Support, the mobile responsive site for the Ministry of Social Development’s Family Services
             Directory.
           </p>
-          <p>
+          <small className="text-muted">
             If you are a provider or want to use the enhanced functions then please visit{' '}
+
             <a href="https://www.familyservices.govt.nz/directory">Family Services Directory</a>.
-          </p>
-        </div>
+          </small>
+        </MDBJumbotron>
       </div>
     </header>
   );
