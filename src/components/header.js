3ec5ff7989ad4a0d6044648a7b69592e7128cc54
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-06-12 16:43:35 +1200

    Escape apostrophe

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <h1>Find Whānau Support - Mobile Site</h1>
         <div>
           <p>
-            This is the mobile responsive site for the Ministry of Social Development's Family Services Directory.
+            This is the mobile responsive site for the Ministry of Social Development&apos;s Family Services Directory.
             Use the site to look for services near you.
           </p>
         </div>
