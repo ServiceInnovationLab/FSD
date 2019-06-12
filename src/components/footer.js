a399af8632a094f6ebd75f531915cee05452f65a
Brenda <brenda@wallace.net.nz>
2019-06-12 16:38:24 +1200

    Links updated in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,23 +7,26 @@ export default class Footer extends Component {
     return (
       <footer className="app__footer">
         <div className="container">
+          <div className="footer-logo">
+            <a href="https://www.govt.nz" className="ga-track-logo-footer-aog footer-hm-link">
+              <img className="footer__logo" src={footerLogo} alt="New Zealand Government" />
+            </a>
+          </div>
           <p>
+            <a href="https://www.digital.govt.nz/blog/labplus-reusable-component-family-services-directory/">About this site</a>
+            {' '}|{' '}
+            <a href="mailto:familyservices@msd.govt.nz">Send us your feedback</a>
+            {' '}|{' '}
+            Data sourced from <a href="https://data.govt.nz">data.govt.nz</a>
+            {' '}|{' '}
+            <a href="https://www.familyservices.govt.nz/directory/providerapplication.htm">Apply to list on the Family Services Directory</a>
+            {' '}|{' '}
             Data sourced from <a href="https://data.govt.nz">data.govt.nz</a>
           </p>
           <p>
             Another alpha from the{' '}
             <a href="https://webtoolkit.govt.nz/blog/tag/service-innovation-lab/">Service Innovation Lab</a>
           </p>
-
-          <p>
-            Please find the full directory or add your services at{' '}
-            <a href="//familyservices.govt.nz/directory">familyservices.govt.nz/directory</a>
-          </p>
-          <div>
-            <a href="https://www.govt.nz" className="ga-track-logo-footer-aog footer-hm-link">
-              <img className="footer__logo" src={footerLogo} alt="New Zealand Government" />
-            </a>
-          </div>
         </div>
       </footer>
     );
