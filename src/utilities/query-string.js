c266721ec9670d37c9e29d599f5cf9cd077f3be6
Awesome Code <team@xinminlabs.com>
2019-08-08 20:18:43 +0000

    Auto corrected by following Lint Javascript unicorn/no-for-loop

--- src/utilities/query-string.js
+++ src/utilities/query-string.js
@@ -5,8 +5,8 @@ export default {
       ? queryString.substr(1)
       : queryString
     ).split('&');
-    for (let i = 0; i < pairs.length; i++) {
-      const pair = pairs[i].split('=');
+    for (const element of pairs) {
+      const pair = element.split('=');
       query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
     }
     return query;
