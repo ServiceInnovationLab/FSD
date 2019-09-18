37e0391d934e2502a0c026434d4fff3e394f671e
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-09-19 11:05:29 +1200

    Added proptypes to ServiceMapMarker

--- src/components/service-map-marker.js
+++ src/components/service-map-marker.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const ServiceMapMarker = props => {
-  const { record } = props;
-  const { FSD_ID, LATITUDE, LONGITUDE, PROVIDER_NAME, ORGANISATION_PURPOSE, PHYSICAL_ADDRESS} = record;
+export default class ServiceMapMarker extends Component {
+  static propTypes = {
+    FSD_ID: PropTypes.string,
+    LATITUDE: PropTypes.string,
+    LONGITUDE: PropTypes.string,
+    PROVIDER_NAME: PropTypes.string,
+    ORGANISATION_PURPOSE: PropTypes.string,
+    PHYSICAL_ADDRESS: PropTypes.string
+  };
 
-  const position = { lat: Number(LATITUDE), lng: Number(LONGITUDE) };
+  render() {
+    const { record } = this.props;
+    const { FSD_ID, LATITUDE, LONGITUDE, PROVIDER_NAME, ORGANISATION_PURPOSE, PHYSICAL_ADDRESS} = record;
 
-  return (
-    <div>
-      {LATITUDE && LONGITUDE && (
-        <Marker position={position}>
-          <Popup>
-            <span>
-              <h5>
-                <Link to={{pathname: `/service/${FSD_ID}`}}>
-                  {PROVIDER_NAME}
-                </Link>
-              </h5>
-              <p>{ORGANISATION_PURPOSE}</p>
-              <p>{PHYSICAL_ADDRESS}</p>
-            </span>
-          </Popup>
-        </Marker>
-      )}
-    </div>
-  );
+    const position = { lat: Number(LATITUDE), lng: Number(LONGITUDE) };
+
+    return (
+      <div>
+        {LATITUDE && LONGITUDE && (
+          <Marker position={position}>
+            <Popup>
+              <span>
+                <h5>
+                  <Link to={{pathname: `/service/${FSD_ID}`}}>
+                    {PROVIDER_NAME}
+                  </Link>
+                </h5>
+                <p>{ORGANISATION_PURPOSE}</p>
+                <p>{PHYSICAL_ADDRESS}</p>
+              </span>
+            </Popup>
+          </Marker>
+        )}
+      </div>
+    );
+  }
 };
-
-export default ServiceMapMarker;
