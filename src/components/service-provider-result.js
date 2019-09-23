4c230f13b413350f0ead39fd5f7b1d4edfabce8e
Merrin Macleod <merrin.macleod@gmail.com>
2019-09-23 15:55:17 +1200

    IE11 css fixes (#396)
    * IE11 css fixes
    
    * Update service-provider-result.js
    
    * Update service-provider-result.js
    
    * white directly
    
    * remove duplicated classes

--- src/components/service-provider-result.js
+++ src/components/service-provider-result.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
 
 import ServiceHeader from './service-header';
 import SaveContact from './save-contact';
@@ -41,11 +40,9 @@ export default class ServiceProviderResult extends Component {
         <div>
         </div>
         <CardActions>
-          <Button color="primary" variant="contained">
-            <Link to={`/service/${mappedProvider.fsdId}`}>
-              More details
-            </Link>
-          </Button>
+          <Link to={`/service/${mappedProvider.fsdId}`} className="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-contained MuiButton-containedPrimary">
+            More details
+          </Link>
           <SaveContact
             name={mappedProvider.name}
             phoneNumber={mappedProvider.phone}
