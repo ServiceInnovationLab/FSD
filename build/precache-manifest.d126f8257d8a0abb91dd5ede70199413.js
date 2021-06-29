5ae98922967bbd62a6c95ba22873984f8b23f7df
Rick <rick.avlonitis@gmail.com>
2021-06-29 19:52:57 +1200

    committing /build to fix broken CircleCi deploy

---
+++ build/precache-manifest.d126f8257d8a0abb91dd5ede70199413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01f22076f913504c5c7841a7d2317d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3121f59f099f1628b69d",
+    "url": "/static/css/main.3b2c0d49.chunk.css"
+  },
+  {
+    "revision": "ae0566c65e19f60018af",
+    "url": "/static/js/2.9c812a93.chunk.js"
+  },
+  {
+    "revision": "bd28da17dab9baf9acb5e2455557e0f8",
+    "url": "/static/js/2.9c812a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3121f59f099f1628b69d",
+    "url": "/static/js/main.517b9b7d.chunk.js"
+  },
+  {
+    "revision": "18a93861a3ce4265928c",
+    "url": "/static/js/runtime-main.69b203b1.js"
+  }
+]);
