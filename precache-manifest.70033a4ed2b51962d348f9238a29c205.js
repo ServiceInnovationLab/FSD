eb2c583a14c52c81a0eb3725261f98003ba85a1a
Rick <rick.avlonitis@gmail.com>
2021-06-30 11:46:21 +1200

    Updates

---
+++ precache-manifest.70033a4ed2b51962d348f9238a29c205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e9ac1e13dfd33df0f3df4f7c1aca44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ac0bc7f5a65db99bf8",
+    "url": "/static/css/main.3b2c0d49.chunk.css"
+  },
+  {
+    "revision": "ae0566c65e19f60018af",
+    "url": "/static/js/2.9c812a93.chunk.js"
+  },
+  {
+    "revision": "bd28da17dab9baf9acb5e2455557e0f8",
+    "url": "/static/js/2.9c812a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ac0bc7f5a65db99bf8",
+    "url": "/static/js/main.7ece8925.chunk.js"
+  },
+  {
+    "revision": "18a93861a3ce4265928c",
+    "url": "/static/js/runtime-main.69b203b1.js"
+  }
+]);
